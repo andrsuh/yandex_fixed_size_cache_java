fd43a19ff4d3a9624e41a0b3560969745f2cdfb4
andrsuh <andrsuh@rambler.ru>
2016-05-08 22:33:59 +0300

    modifer final for some variable was added

--- src/main/java/andrey/yandex/LFUCache.java
+++ src/main/java/andrey/yandex/LFUCache.java
@@ -4,24 +4,43 @@ import java.util.*;
 import java.util.AbstractMap.SimpleEntry;
 
 /**
- * Created by andrey on 04.05.16.
+ * LFU cache implementation based on priority queue provides
+ * O(log n) speed for get, put operations
  */
 
 public class LFUCache<K, V> extends AbstractFixedSizeCache<K, V> {
     private final Map<K, Integer> frequencies = new HashMap<>(); // store key and its actually frequency
-    private final Queue<Map.Entry<Integer, K>> frequencies_queue;
+    private final Queue<Map.Entry<Integer, K>> frequenciesQueue;
+
+    /**
+     * Empty constructor set maximum cache capacity = DEFAULT_CAPACITY
+     */
 
     public LFUCache() {
         this(DEFAULT_CAPACITY);
     }
 
+    /**
+     * Create priority queue will store key and its frequency
+     * Queue provides O(1) speed for getting key with min. freq
+     *
+     * @param capacity maximum cache capacity
+     */
+
     public LFUCache(int capacity) {
         super(new HashMap<>(), capacity);
 
-        frequencies_queue = new PriorityQueue<>(
-                capacity, (o1, o2) -> o1.getKey().compareTo(o2.getKey()) // will store key and its freq. sorted by freq. min
+        frequenciesQueue = new PriorityQueue<>(
+                capacity, (o1, o2) -> o1.getKey().compareTo(o2.getKey())
         );
     }
+
+    /**
+     * In LFU pulling strategy every times we want to get value
+     * associated with key we must increase key frequency.
+     *
+     * @inheritDoc
+     */
 
     @Override
     public V get(K key) {
@@ -34,10 +53,10 @@ public class LFUCache<K, V> extends AbstractFixedSizeCache<K, V> {
 
             hits.incrementAndGet();
 
-            int currentFrequency = frequencies.get(key); // get actually frequency for key
+            final int currentFrequency = frequencies.get(key); // get actually frequency for key
 
-            frequencies_queue.remove(new SimpleEntry<>(currentFrequency, key)); // remove from queue old values
-            frequencies_queue.offer(new SimpleEntry<>(currentFrequency + 1, key)); // set actually values in queue
+            frequenciesQueue.remove(new SimpleEntry<>(currentFrequency, key)); // remove from queue old values
+            frequenciesQueue.offer(new SimpleEntry<>(currentFrequency + 1, key)); // set actually values in queue
             frequencies.put(key, currentFrequency + 1); // and update it in out map
 
             return cache.get(key);
@@ -45,6 +64,13 @@ public class LFUCache<K, V> extends AbstractFixedSizeCache<K, V> {
             lock.writeLock().unlock();
         }
     }
+
+    /**
+     * Before addition if current cache size great then maximal capacity
+     * key with minimal frequency will remove
+     *
+     * @inheritDoc
+     */
 
     @Override
     public void put(K key, V value) {
@@ -56,14 +82,14 @@ public class LFUCache<K, V> extends AbstractFixedSizeCache<K, V> {
 
             cache.put(key, value);
             frequencies.put(key, 1); // initially key has frequency == 1
-            frequencies_queue.offer(new SimpleEntry<>(1, key));
+            frequenciesQueue.offer(new SimpleEntry<>(1, key));
         } finally {
             lock.writeLock().unlock();
         }
     }
 
     private void remove() {
-        Map.Entry<Integer, K> pair = frequencies_queue.poll(); // get key with smallest freq.
+        final Map.Entry<Integer, K> pair = frequenciesQueue.poll(); // get key with smallest freq.
 
         cache.remove(pair.getValue());
         frequencies.remove(pair.getValue());
